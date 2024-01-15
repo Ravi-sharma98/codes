6be221f10b7e4a3e58d33093d5b2ab8740174079
Ravi-sharma98 <156676632+Ravi-sharma98@users.noreply.github.com>
2024-01-15 21:31:27 +0530

    Add files via upload

---
+++ Datascienceecosystem.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Datascience tools and ecosystem
+
+# # In this notebook datascience tools and ecosystems are summarized
+
+# # Objectives:
+# 
+# + Languages of Datascience
+# + Libraries used in data science
+# + Tools of datascience
+
+# # Some of the popular data science languages are:
+# 1. Python
+# 2. R
+# 3. Julia
+
+# # Some of the commonly used libraries used by datascientist include:
+# 1. Numpy
+# 2. Pandas
+# 3. Matplotlib
+# 4. tensorflow
+# 5. pyplot
+
+# | Data Science Tools |
+# | ------------------ |
+# | Hadoop             |
+# | Python             |
+# | R                  |
+# | Jupyter Notebooks  |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+#This is a simple arithmetic expression to add and multiply integers
+a=3
+b=4
+c=5
+z=(a*b)+5
+print(z)
+
+
+# In[5]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+a=200
+b=200//60
+print(b)
+
+
+# ## Author
+# Ravi Sharma
+
+# In[ ]:
+
+
+
+
